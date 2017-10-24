1e40777cf3c569eb455eb3625dd60a0408b676f5
jinkyu2005 <jinkyu2005@naver.com>
2017-10-24 16:02:26 +0900

    paging sucess 2017-10-24

--- controllers/CreateContentController.js
+++ controllers/CreateContentController.js
@@ -1,12 +1,12 @@
 module.exports = function(app, sqlConnection)
 {
     app.get('/createNewPostPopup', function(request, response){	  
-		response.render('CreateNewPostPopup.html');
-		console.log('/createNewPostPopup');
+			response.render('CreateNewPostPopup.html');
+			console.log('/createNewPostPopup');
     });
     app.post('/createContentAction', function(request, response){
-		var model = require('../models/CreateContentActionModel.js');	
-		model.render(request, response, sqlConnection);
+			var model = require('../models/CreateContentActionModel.js');	
+			model.render(request, response, sqlConnection);
 	});
 }
 
